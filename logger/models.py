aebe1845dab6e5a27935562dd16ccc8034bdeab6
TheRealMcCall <lamccall1983@gmail.com>
2025-07-16 13:20:24 +0100

    Change models.py for better readability in admin panel

--- logger/models.py
+++ logger/models.py
@@ -3,12 +3,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+# Model for DayLog
 class DayLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
 
+
+# adjusts how DayLog objects are shown in admin
+def __str__(self):
+    return f"{self.user.username} - {self.start_date}"
+
+
+# Model for individual trips
 class Trip(models.Model):
-    day_log = models.ForeignKey(DayLog, on_delete=models.CASCADE, related_name ='trips')
+    day_log = models.ForeignKey(DayLog, on_delete=models.CASCADE, related_name='trips')
     start_time = models.TimeField()
     finish_time = models.TimeField()
     is_overnight = models.BooleanField(default=False)
