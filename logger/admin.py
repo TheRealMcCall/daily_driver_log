9f2ed4fd84eb7917e531966318b24031ae5ba511
TheRealMcCall <lamccall1983@gmail.com>
2025-07-16 13:07:47 +0100

    Add Trip model and registered in admin.py

--- logger/admin.py
+++ logger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import DayLog
+from .models import Trip
 # Register your models here.
 
 admin.site.register(DayLog)
+admin.site.register(Trip)
--- logger/models.py
+++ logger/models.py
@@ -6,3 +6,9 @@ from django.contrib.auth.models import User
 class DayLog(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     start_date = models.DateField()
+
+class Trip(models.Model):
+    day_log = models.ForeignKey(DayLog, on_delete=models.CASCADE, related_name ='trips')
+    start_time = models.TimeField()
+    finish_time = models.TimeField()
+    is_overnight = models.BooleanField(default=False)
