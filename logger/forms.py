b5e8bef3c56b8e5e628026db7986d8ffe04b6632
TheRealMcCall <lamccall1983@gmail.com>
2025-07-20 11:56:29 +0100

    Refactor: forms.py reduced line length of code

--- logger/forms.py
+++ logger/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Trip, DayLog
 
+
 # Form to create or update a trip.
 class TripForm(forms.ModelForm):
     class Meta:
@@ -8,10 +9,17 @@ class TripForm(forms.ModelForm):
         model = Trip
         fields = ['trip_start_time', 'trip_finish_time', 'is_overnight']
         widgets = {
-            'trip_start_time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
-            'trip_finish_time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control'}),
-            'is_overnight': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'trip_start_time': forms.TimeInput(
+                attrs={'type': 'time', 'class': 'form-control'}
+                 ),
+            'trip_finish_time': forms.TimeInput(
+                attrs={'type': 'time', 'class': 'form-control'}
+                 ),
+            'is_overnight': forms.CheckboxInput(
+                attrs={'class': 'form-check-input'}
+                ),
         }
+
 
 # Form to create a Daylog.
 class DayLogForm(forms.ModelForm):
@@ -20,5 +28,6 @@ class DayLogForm(forms.ModelForm):
         model = DayLog
         fields = ['start_date']
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'start_date': forms.DateInput(
+                attrs={'type': 'date', 'class': 'form-control'}),
         }
